11020a1b0a9d43528dc8cc378479ea52d0bc5b78
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-05-02 12:17:29 -0300

    feat(auth): add auth route and service with jwt

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '665f02dc8f79ddf20c4b64c478f149f7', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    delete user.password;
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
