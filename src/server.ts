1bdc533f984ea8dd1fab78f716e235eaa7ddac2e
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-05-04 01:47:07 -0300

    feat(error_handling): add global handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
-import express from 'express';
-import routes from './routes';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppErrors';
 
 const app = express();
 const PORT = 3333;
@@ -12,6 +14,22 @@ const PORT = 3333;
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (err: Error, resquest: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`🚀️ Running server on port ${PORT}`);
