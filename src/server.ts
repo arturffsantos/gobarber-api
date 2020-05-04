22d47026648fcaff176c2b2993786b1a9b5202c0
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-05-04 01:12:41 -0300

    feat(users): serving avatar file

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(PORT, () => {
