38a71cacc969bacf7a1bf698f791e680d4f210cd
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-07-23 21:50:44 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,6 +12,7 @@ import AppError from './errors/AppErrors';
 const app = express();
 const PORT = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
