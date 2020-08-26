870c6a14bd7f3a60f23e753accde365b3c0533cd
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-08-26 19:10:44 -0300

    test create two appointments in same date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { startOfHour } from 'date-fns';
 
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
@@ -24,5 +25,28 @@ describe('CreateAppointment', () => {
     expect(appointment.created_at).toBeTruthy();
     expect(appointment.updated_at).toBeTruthy();
     expect(appointment.created_at).toEqual(appointment.updated_at);
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    const appointment = await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
