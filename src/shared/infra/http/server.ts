6030154a9b058c0125a929da4e1e04a045fec471
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-08-26 21:58:57 -0300

    add storage providers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,7 +15,7 @@ const PORT = 3333;
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use(
