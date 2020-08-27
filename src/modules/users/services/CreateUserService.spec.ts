d2db92301a87da284a533b81d0497b16f023af0a
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-08-26 21:07:53 -0300

    update tests on CreateUserService to use FakeHashProvider

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -3,11 +3,17 @@ import 'reflect-metadata';
 import AppError from '@shared/errors/AppError';
 import CreateUserService from './CreateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/hashProvider/fakes/FakeHashProvider';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUserService = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUserService.execute({
       name: 'John Doe',
@@ -26,7 +32,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with duplicated email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUserService = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUserService.execute({
       name: 'John Doe',
